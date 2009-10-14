adf3a19907b2ec8050f54fdd4bd268e96578825d
riverbird2005 <riverbird2005@e8f56e28-abf9-11de-a87c-5360b77f26dc>
2009-10-14 10:11:15 +0000

    1). update web;

--- trunk/todoshell/models/todo.py
+++ trunk/todoshell/models/todo.py
@@ -2,7 +2,7 @@
 # try something like
 import datetime
 now = datetime.date.today()
-todo=SQLDB("sqlite://todo.db")
+db=SQLDB("sqlite://todo.db")
 db.define_table('queue',
                 SQLField('name'))
 db.define_table('task',
@@ -10,14 +10,13 @@ db.define_table('task',
                 SQLField('end_time','date',default=now),
                 SQLField('subject',length=256),
                 SQLField('comment',length=256),
-                SQLField('status',default=0),
-                SQLField('queue',db.queue),
-                
+                SQLField('status','integer',default=0,length=1),
+                SQLField('queue_id',db.queue),
                 )
 db.queue.name.requires=[IS_NOT_EMPTY(),IS_NOT_IN_DB(db,'queue.name')]
 
 db.task.subject.requires=[IS_NOT_EMPTY()]
 db.task.create_time.requires=IS_NOT_EMPTY()
-db.task.queue.requires=IS_IN_DB(db,'queue.id','queue.name')
+db.task.queue_id.requires=IS_IN_DB(db,'queue.id','queue.name')
 db.task.create_time.requires=IS_DATE()
 db.task.end_time.requires=IS_DATE()
